ae5f38015c2f66a3dca413a9d23bde7d6a66ce1c
Neal Donnelly <nealjmd@gmail.com>
2014-02-04 14:43:09 -0500

    polished flood_fill and added tests

--- tests/test_agglo.py
+++ tests/test_agglo.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.testing import assert_equal, assert_array_equal
 
-from gala import agglo
+from gala import agglo, morpho
 
 
 test_idxs = range(4)
@@ -51,6 +51,30 @@ def test_no_dam_agglomeration():
     g.agglomerate(0.75)
     assert_array_equal(g.get_segmentation(), results[i],
                        'No dam agglomeration failed.')
+
+
+def test_flood_fill():
+    fail_message = 'Flood fill failed.'
+    example = np.array([[[0,1,2,5],
+                         [0,0,2,4],
+                         [1,0,1,2]],
+                        [[0,0,5,5],
+                         [1,1,1,5],
+                         [1,2,1,5]]])
+    t1 = morpho.flood_fill(example, (0,0,0), [0], True)
+    assert_equal(set(t1), set([0,4,5,9,12,13]), fail_message)
+    t2 = morpho.flood_fill(example, (0,0,0), [0], False)
+    assert_equal(set(t2), set([(0,0,0), (0,1,0), (0,1,1), (0,2,1), (1,0,0),
+                            (1,0,1)]), fail_message)
+    t3 = morpho.flood_fill(example, (0,1,3), [2], True)
+    assert_equal(set(t3), set([]), fail_message)
+    t4 = morpho.flood_fill(example, (0,0,3), [5], True)
+    assert_equal(set(t4), set([3,14,15,19,23]), fail_message)
+    t5 = morpho.flood_fill(example, (1,1,1), [1,4], True)
+    assert_equal(set(t5), set([8,10,16,17,18,20,22]), fail_message)
+    t6 = morpho.flood_fill(example, (0,1,2), [2,5], True)
+    assert_equal(set(t6), set([2,3,6,11,14,15,19,23]), fail_message)
+
 
 if __name__ == '__main__':
     from numpy import testing
